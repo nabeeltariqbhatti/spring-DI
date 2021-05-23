c920672af32e33b92a54b66a96a61689c77116b2
nabeeltaariq <nabeeltaariq@gmail.com>
2021-05-23 08:25:09 -0700

    adding with first mvc webapp in bootstrap

---
+++ first/src/main/java/com/example/demo/Laptop.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+	private int id;
+	private String brand;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	public void complie() {
+		System.out.println("Compiling.....");
+	}
+	@Override
+	public String toString() {
+		return "Laptop [id=" + id + ", brand=" + brand + "]";
+	}
+	
+	
+	
+	
+
+}
