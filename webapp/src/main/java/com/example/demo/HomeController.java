1c0565dfa797241604ce3c8dbc6adbceea35536a
nabeeltaariq <nabeeltaariq@gmail.com>
2021-05-24 08:01:08 -0700

    Model and view instead of servlets

--- webapp/src/main/java/com/example/demo/HomeController.java
+++ webapp/src/main/java/com/example/demo/HomeController.java
@@ -10,20 +10,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/home")
 
-	public String getFoosBySimplePath(@RequestParam("name")String name, HttpSession session) {
+	public ModelAndView getFoosBySimplePath(@RequestParam("name")String name) {
 		
-	
-		System.out.println("hi " + " " + name );
+	    ModelAndView mv =new ModelAndView();
+		mv.addObject("name",name);
+		mv.setViewName("home.jsp");
 		
-		session.setAttribute("name", name);
-		
-	    return "home.jsp";
+	    return mv;
 	}
 	
 	
