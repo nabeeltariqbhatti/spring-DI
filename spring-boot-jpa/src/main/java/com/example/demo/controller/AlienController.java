0c4ab7276025336f418681f0d4c4c35d16e395d5
nabeeltaariq <nabeeltaariq@gmail.com>
2021-05-26 06:52:32 -0700

    custom methods to interact with data

--- spring-boot-jpa/src/main/java/com/example/demo/controller/AlienController.java
+++ spring-boot-jpa/src/main/java/com/example/demo/controller/AlienController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.AlienRepo;
 import com.example.demo.model.Alien;
@@ -22,6 +24,20 @@ public class AlienController {
 	public String addAlien(Alien alien) {
 		repo.save(alien);
 		return "home.jsp";
+	}
+	
+	@RequestMapping("/getAlien")
+	public ModelAndView getAlien(@RequestParam int aid) {
+		
+		
+		ModelAndView mv = new ModelAndView("showAlien.jsp");
+		
+		Alien alien = repo.findById(aid).orElse(new Alien());
+		mv.addObject(alien);
+		
+		System.out.println(repo.findByTech("java"));
+		System.out.println(repo.findByAidGreaterThan(102));
+		return mv;
 	}
 
 }
