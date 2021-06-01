e2333282ad3b4f56bdc33c4a328fc320b3f14d12
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-06-01 16:10:38 +0500

    Spring boot web services

---
+++ rest-spring-practice/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,88 @@
+package com.example.demo.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.UserRest;
+import com.example.demo.request.UserDetail;
+
+@RestController
+@RequestMapping("users") // https://localhost:8080/users
+public class UserController {
+
+	Map<String, UserRest> users;
+
+	@GetMapping
+	public String getUsers(@RequestParam(value = "name", defaultValue = "Unknown", required = true) String name) {
+		return "all the user have been returned with name " + name;
+	}
+
+	@GetMapping(path = "/{userId}", produces = { MediaType.APPLICATION_ATOM_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
+		if (users != null) {
+			return new ResponseEntity<UserRest>(users.get(userId), HttpStatus.FOUND);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+
+	}
+
+	@PostMapping(consumes = { MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+			MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<UserRest> craeteUser(@Valid @RequestBody UserDetail userDetail) {
+
+		UserRest returnValue = new UserRest();
+
+		returnValue.setFirstName(userDetail.getFirstName());
+		returnValue.setLastName(userDetail.getLastName());
+		returnValue.setEmail(userDetail.getEmail());
+		String userId = UUID.randomUUID().toString();
+		returnValue.setUserId(userId);
+
+		if (users == null) {
+			users = new HashMap<>();
+			users.put(userId, returnValue);
+		} else {
+			users.put(userId, returnValue);
+		}
+		return new ResponseEntity<UserRest>(returnValue, HttpStatus.CREATED);
+	}
+
+	@PutMapping(value = "/{userId}", consumes = { MediaType.APPLICATION_ATOM_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<UserRest> updateUser(@PathVariable String userId, @RequestBody UserRest userRest) {
+		if (users.get(userId) != null) {
+			users.replace(userId, users.get(userId), userRest);
+			UserRest returnValue = users.get(userId);
+			returnValue.setUserId(userId);
+
+			return new ResponseEntity<UserRest>(returnValue, HttpStatus.ACCEPTED);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+		}
+	}
+
+	@DeleteMapping
+
+	public String deleteUser() {
+		return "delete user was called";
+	}
+}
---
+++ rest-spring-practice/src/main/java/com/example/demo/request/UserDetail.java
@@ -0,0 +1,57 @@
+package com.example.demo.request;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserDetail {
+
+	@NotNull(message = "First Name is required")
+	private String firstName;
+	@NotNull(message = "Last Name is required")
+	private String lastName;
+	@NotNull(message = "Email is required")
+	@Email
+	private String email;
+	@NotNull(message = "Password Name is required")
+	@Size(min = 8, max = 16)
+	private String password;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@JsonIgnore
+	@JsonProperty(value = "password")
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
