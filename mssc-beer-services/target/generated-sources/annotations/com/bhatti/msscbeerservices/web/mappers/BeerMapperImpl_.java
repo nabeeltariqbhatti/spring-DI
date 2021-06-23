2bfb3b14876d2a8e85712688a901c8e498b20d14
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-06-23 10:01:43 +0500

    Running condition mss-beer-service

--- mssc-beer-services/target/generated-sources/annotations/com/bhatti/msscbeerservices/web/mappers/BeerMapperImpl_.java
+++ mssc-beer-services/target/generated-sources/annotations/com/bhatti/msscbeerservices/web/mappers/BeerMapperImpl_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-06-19T16:29:03+0500",
+    date = "2021-06-22T16:28:37+0500",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 16 (Oracle Corporation)"
 )
 public class BeerMapperImpl_ implements BeerMapper {
